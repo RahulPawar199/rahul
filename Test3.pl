5e100fe5ac747c047d4ed849b99eccde4b7d5c37
RahulPawar199 <130059411+RahulPawar199@users.noreply.github.com>
2024-12-11 16:05:56 +0530

    Create Test3.pl
    Test

---
+++ Test3.pl
@@ -0,0 +1,9 @@
+my $number = -5;
+
+if ($number > 0) {
+    print "The number is positive.\n";
+} elsif ($number < 0) {
+    print "The number is negative.\n";
+} else {
+    print "The number is zero.\n";
+}

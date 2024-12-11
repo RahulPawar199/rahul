982874b83895a7012452ca3ccac42fdc7633196f
RahulPawar199 <130059411+RahulPawar199@users.noreply.github.com>
2024-12-11 11:13:07 +0530

    Create TestPython.py
    Test

---
+++ TestPython.py
@@ -0,0 +1,10 @@
+print("Enter first number: ")
+num1 = input()
+num1 = num1.strip()
+
+print("Enter second number: ")
+num2 = input()
+num2 = num2.strip()
+
+sum = int(num1) + int(num2)
+print(f"The sum of {num1} and {num2} is {sum}")

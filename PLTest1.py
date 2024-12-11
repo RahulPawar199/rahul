66b229b76fc212cb1562c9e45a6730081a1c8215
RahulPawar199 <130059411+RahulPawar199@users.noreply.github.com>
2024-12-11 11:14:45 +0530

    Convert Perl file to Python

---
+++ PLTest1.py
@@ -0,0 +1,11 @@
+
+Here's the equivalent Python code:
+```
+# Python code with if-else conditions
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is 5 or less")
+```
+Note that in Python, we use indentation to denote block of code, whereas in Perl, we use curly braces { }. Also, in Python we use "print" function instead of "print" statement.

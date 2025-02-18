c265a9af6c93d3fb6eed383b53b74fca586bba62
RahulPawar199 <130059411+RahulPawar199@users.noreply.github.com>
2025-02-18 14:06:40 +0530

    Convert Perl file to Python

---
+++ Testdeeply3.py
@@ -0,0 +1,19 @@
+Here is the Python code to convert:
+```
+#!/usr/bin/env python
+
+x = 10
+y = 20
+
+if x > 5:
+    if y > 15:
+        if x + y > 30:
+            print("Both x and y are large, and their sum is greater than 30.")
+        else:
+            print("x and y are large, but their sum is not greater than 30.")
+    else:
+        print("x is large, but y is not greater than 15.")
+else:
+    print("x is not large.")
+```
+Note that in Python, we use indentation to define blocks of code instead of curly braces. Also, in Python, we use the `print` function to display output on the screen, while in Perl, we use `print` as a statement.

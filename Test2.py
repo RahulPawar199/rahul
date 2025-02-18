222d91aab64e2c4ff6d0e20059bb19e0086fd79a
RahulPawar199 <130059411+RahulPawar199@users.noreply.github.com>
2025-02-18 14:06:33 +0530

    Convert Perl file to Python

---
+++ Test2.py
@@ -0,0 +1,11 @@
+Here is the equivalent Python code:
+```
+from __future__ import print_function
+
+$number = 10
+
+if $number % 2 == 0:
+    print("The number {} is even.".format($number))
+else:
+    print("The number {} is odd.".format($number))
+```
